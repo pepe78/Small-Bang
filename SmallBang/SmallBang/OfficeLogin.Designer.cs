8d6631ffe6db699682c8656c0c434703e160d196
pepe78 <taraba.pepe@gmail.com>
2017-05-05 17:58:41 -0700

    Making window sizeable and movable in case Microsoft continues to misbehave ;)

--- SmallBang/SmallBang/OfficeLogin.Designer.cs
+++ SmallBang/SmallBang/OfficeLogin.Designer.cs
@@ -48,7 +48,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(726, 454);
             this.Controls.Add(this.webBrowser1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "OfficeLogin";
             this.Text = "OfficeLogin";
